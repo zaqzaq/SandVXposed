9a786b55a4dd092eed8e130f419a23ad5d6c3c37
zhangyingjie <zhangyingjie@xubei.com>
2019-04-12 17:51:25 +0800

    先让c++代码编译通过

---
+++ hookers/src/main/java/com/swift/sandhook/hookers/ObjectHooker.java
@@ -0,0 +1,25 @@
+package com.swift.sandhook.hookers;
+
+import android.util.Log;
+
+import com.swift.sandhook.annotation.HookClass;
+import com.swift.sandhook.annotation.HookMethod;
+import com.swift.sandhook.annotation.HookMethodBackup;
+import com.swift.sandhook.annotation.HookMode;
+
+@HookClass(Object.class)
+public class ObjectHooker {
+
+    @HookMethod("toString")
+    @HookMode(HookMode.INLINE)
+    public static String toStr(Object thiz) {
+        Log.e("ObjectHooker", "hooked success ");
+        return toStrBackup(thiz);
+    }
+
+    @HookMethodBackup("toString")
+    public static String toStrBackup(Object thiz) {
+        return toStrBackup(thiz);
+    }
+
+}
